e6eccf936c8c4715b9aea02a71f99c1fea0d5430
SEAN H F HE (ITA-ISDC-ISD-OOCLL/ZHA) <HESE2@OOCL.COM>
2019-07-12 17:03:31 +0800

    finish add

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -3,10 +3,19 @@ package com.thoughtworks.collection;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Add {
     public int getSumOfEvens(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+//        if (leftBorder < rightBorder) {
+//            return IntStream.rangeClosed(leftBorder, rightBorder).boxed().filter(item -> item % 2 == 0).mapToInt(i -> i).sum();
+//        } else {
+//            return IntStream.rangeClosed(rightBorder, leftBorder).boxed().filter(item -> item % 2 == 0).mapToInt(i -> i).sum();
+//        }
+        int left = Integer.min(leftBorder, rightBorder);
+        int right = Integer.max(leftBorder, rightBorder);
+        return IntStream.range(left,right+1).filter(x->x%2==0).sum();
+
     }
 
     public int getSumOfOdds(int leftBorder, int rightBorder) {
@@ -14,6 +23,7 @@ public class Add {
     }
 
     public int getSumTripleAndAddTwo(List<Integer> arrayList) {
+
         throw new NotImplementedException();
     }
 
