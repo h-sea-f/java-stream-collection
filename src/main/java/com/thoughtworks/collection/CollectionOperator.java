5195a831298707b3c95c12b232f9c586ec584dfc
h-sea-f <h_sea_f@163.com>
2019-07-13 11:17:54 +0800

    finish getListByInterval

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -2,11 +2,19 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class CollectionOperator {
     public List<Integer> getListByInterval(int left, int right) {
-        throw new NotImplementedException();
+        if (left < right) {
+            return IntStream.rangeClosed(left, right).boxed().collect(Collectors.toList());
+        } else {
+            return IntStream.rangeClosed(right, left).boxed().sorted(Comparator.reverseOrder()).collect(Collectors.toList());
+        }
+
     }
 
     public List<Integer> getEvenListByIntervals(int left, int right) {
