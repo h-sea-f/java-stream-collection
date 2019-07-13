672d59bc554f4d01f3368e9c493cbf364001afe3
h-sea-f <h_sea_f@163.com>
2019-07-13 15:21:59 +0800

    finish mapLetter

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -22,7 +22,7 @@ public class MyMap {
     }
 
     public List<String> mapLetter() {
-        throw new NotImplementedException();
+        return array.stream().map(item -> letterList.get(item - 1)).collect(Collectors.toList());
     }
 
     public List<String> mapLetters() {
