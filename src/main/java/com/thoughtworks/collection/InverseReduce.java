87cba68866d8bd9e82328b80a34bc20e4ba3f644
h-sea-f <h_sea_f@163.com>
2019-07-13 15:06:28 +0800

    finish divideToSmaller

--- src/main/java/com/thoughtworks/collection/InverseReduce.java
+++ src/main/java/com/thoughtworks/collection/InverseReduce.java
@@ -2,20 +2,30 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class InverseReduce {
 
     private Random random = new Random();
 
-    public InverseReduce() {}
+    public InverseReduce() {
+    }
 
     public InverseReduce(Random random) {
         this.random = random;
     }
 
     public List<Integer> divideToSmaller(int number) {
-        throw new NotImplementedException();
+        if (number % 2 == 0) {
+            return IntStream.rangeClosed(0, number - 2).boxed().sorted(Comparator.reverseOrder()).filter(item -> item % 2 == 0).collect(Collectors.toList());
+        } else {
+            return IntStream.rangeClosed(1, number - 2).boxed().sorted(Comparator.reverseOrder()).filter(item -> item % 2 != 0).collect(Collectors.toList());
+        }
+
     }
 }
