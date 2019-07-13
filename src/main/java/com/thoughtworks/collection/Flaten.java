b539054349d368fe3505fe842f8bd320da6331e4
h-sea-f <h_sea_f@163.com>
2019-07-13 14:52:41 +0800

    finish transformToUnrepeatedOneDimesional

--- src/main/java/com/thoughtworks/collection/Flaten.java
+++ src/main/java/com/thoughtworks/collection/Flaten.java
@@ -19,6 +19,6 @@ public class Flaten {
     }
 
     public List<Integer> transformToUnrepeatedOneDimesional() {
-        throw new NotImplementedException();
+        return Arrays.stream(array).flatMap(Arrays::stream).distinct().collect(Collectors.toList());
     }
 }
--- src/test/java/com/thoughtworks/collection/FlatenTest.java
+++ src/test/java/com/thoughtworks/collection/FlatenTest.java
@@ -22,15 +22,15 @@ public class FlatenTest {
         assertThat(flaten.transformToOneDimesional()).isEqualTo(resultList);
     }
 //
-//    @Test
-//    public void can_transform_two_dimensional_array_to_one_and_delete_repeated() {
-//        // 把二维数组变成一维数组,消除重复,按照第一次出现的顺序排列最后的输出结果
-//        Integer[][] array = {{1, 2, 3}, {5, 2, 1, 4}, {2, 1}};
-//
-//        Integer[] result = {1, 2, 3, 5, 4};
-//        List<Integer> resultList = Arrays.asList(result);
-//
-//        Flaten flaten = new Flaten(array);
-//        assertThat(flaten.transformToUnrepeatedOneDimesional()).isEqualTo(resultList);
-//    }
+@Test
+public void can_transform_two_dimensional_array_to_one_and_delete_repeated() {
+    // 把二维数组变成一维数组,消除重复,按照第一次出现的顺序排列最后的输出结果
+    Integer[][] array = {{1, 2, 3}, {5, 2, 1, 4}, {2, 1}};
+
+    Integer[] result = {1, 2, 3, 5, 4};
+    List<Integer> resultList = Arrays.asList(result);
+
+    Flaten flaten = new Flaten(array);
+    assertThat(flaten.transformToUnrepeatedOneDimesional()).isEqualTo(resultList);
+}
 }
