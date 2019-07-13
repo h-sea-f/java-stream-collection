71a6120d9177226b73243b62b368012144f8b34d
h-sea-f <h_sea_f@163.com>
2019-07-13 11:32:16 +0800

    finish popLastElement

--- src/main/java/com/thoughtworks/collection/CollectionOperator.java
+++ src/main/java/com/thoughtworks/collection/CollectionOperator.java
@@ -32,7 +32,8 @@ public class CollectionOperator {
     }
 
     public int popLastElment(int[] array) {
-        throw new NotImplementedException();
+
+        return array[array.length - 1];
     }
 
     public List<Integer> popCommonElement(int[] firstArray, int[] secondArray) {
