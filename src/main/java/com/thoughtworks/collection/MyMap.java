d0d10a7e37dd59dbb0090cec049644db21183abf
h-sea-f <h_sea_f@163.com>
2019-07-13 15:38:22 +0800

    finish sortFromSmall

--- src/main/java/com/thoughtworks/collection/MyMap.java
+++ src/main/java/com/thoughtworks/collection/MyMap.java
@@ -35,6 +35,7 @@ public class MyMap {
     }
 
     public List<Integer> sortFromSmall() {
-        throw new NotImplementedException();
+        return array.stream().sorted().collect(Collectors.toList());
     }
+
 }
